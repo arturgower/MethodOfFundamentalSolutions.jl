9c6c6c1d1898fc4efff63dec3dc19e8c0d4cefce
arturgower <arturgower@gmail.com>
2025-12-06 16:28:38 +0000

    just spaces

--- src/physics/acoustic.jl
+++ src/physics/acoustic.jl
@@ -7,7 +7,7 @@ struct NeumannType <: FieldType end
 
 function greens(field::DirichletType, medium::Acoustic{T,2}, x::SVector{2,T}, outward_normal; ω::Float64= 2pi * 1.0) where T
    
-    G=zeros(Complex{T},1,1)
-    G[1,1]=-(im/4) * hankelh1(0, (ω / medium.c) * norm(x))
+    G = zeros(Complex{T},1,1)
+    G[1,1] = -(im/4) * hankelh1(0, (ω / medium.c) * norm(x))
     return G
 end
